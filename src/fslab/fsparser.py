dae55e1ee8c807eda81cac6a8211c725ca8079f1
Guillem Frances <guillem.frances@gmail.com>
2019-12-12 16:15:44 +0100

    Some minor parsing improvements

--- src/fslab/fsparser.py
+++ src/fslab/fsparser.py
@@ -23,16 +23,21 @@ def solved(run):
 def parse_node_generation_rate(content, props):
     # We do a first parse of the online-printed generation rate, in case it exists,
     # but will overwrite this later if we found the final value in the JSON output.
-    allrates = re.findall(r'generations \(nodes/sec\.\): (.+). Memory consumption', content)
-    props['node_generation_rate'] = float(allrates[-1]) if allrates else 0
+    # [INFO][454.35526] Node generation rate after 5950K generations (nodes/sec.): 13281.5. Memory consumption: 7730092kB. / 7806784 kB.
+    allrates = re.findall(r'generations \(nodes/sec\.\): (.+). Memory consumption: (\d+)kB\.', content)
+    props['node_generation_rate'] = float(allrates[-1][0]) if allrates else 0
+    props['memory'] = float(allrates[-1][1]) if allrates else 0
 
 
-def parse_memory_watchpoints(content, props):
+def parse_memory_time_watchpoints(content, props):
     res = re.findall(r'Mem\. usage before match-tree construction: (\d+)kB\. /', content)
     props['mem_before_mt'] = int(res[-1]) if res else 0
 
     res = re.findall(r'Mem\. usage on start of SBFWS search: (\d+)kB\. /', content)
     props['mem_before_search'] = int(res[-1]) if res else 0
+
+    allrates = re.findall(r'\[INFO\]\[(\d+\.\d+)\]', content)
+    props['last_recorded_time'] = float(allrates[-1]) if allrates else 0
 
 
 def parse_grounding_info(content, props):
@@ -60,7 +65,8 @@ def parse_sdd_minimization(content, props):
 
 def parse_results(content, props):
     # TODO planner_exit_code is still not too reliable
-    props['error'] = 'all-good' if props['planner_exit_code'] == 0 else 'unsolvable-or-error'
+    props['error'] = 'all-good' if 'planner_exit_code' not in props or props['planner_exit_code'] == 0\
+        else 'unsolvable-or-error'
 
     props['coverage'] = 0  # Unless proven otherwise, the instance is assumed not solved
 
@@ -97,7 +103,8 @@ def parse_results(content, props):
     props['unsolvable'] = props['coverage'] == 0 and not props['out_of_memory']
 
     if solved(props):
-        props['memory'] = out['memory']
+        if 'memory' in out:
+            props['memory'] = out['memory']  # Override previous temporary measurements
         props['search_time'] = out['search_time']
         props['total_time'] = out['total_time']
         props['plan_length'] = int(out['plan_length'])
@@ -125,7 +132,7 @@ class FSOutputParser(Parser):
         self.add_pattern('node', r'node: (.+)\n', type=str, file='driver.log', required=True)
         self.add_pattern('planner_exit_code', r'run-planner exit code: (.+)\n', type=int, file='driver.log')
 
-        self.add_function(parse_memory_watchpoints, file="run.log")
+        self.add_function(parse_memory_time_watchpoints, file="run.log")
         self.add_function(parse_grounding_info, file="run.log")
         self.add_function(parse_node_generation_rate, file="run.log")
         self.add_function(parse_sdd_minimization, file="run.log")
